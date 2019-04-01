22f8adf6e399dfac5811ff78e62085aabefdb99e
Courtney Bean <calysabean@gmail.com>
2019-03-31 20:22:46 -0400

    final commit

--- server.js
+++ server.js
@@ -1,42 +1,40 @@
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require("mongoose");
+var passport = require("passport");
+var flash = require("connect-flash");
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var morgan = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
 mongoose.Promise = global.Promise;
 
-const { DATABASE_URL, PORT} = require('./config/database.js');
+const { DATABASE_URL, PORT } = require("./config/database.js");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-require('./config/passport')(passport); // pass passport for configuration
+require("./config/passport")(passport); // pass passport for configuration
 
 // set up our express application
-app.use(morgan('dev')); // log every request to the console
+app.use(morgan("dev")); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set("view engine", "ejs"); // set up ejs for templating
 
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: "ilovescotchscotchyscotchscotch" })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-
-require('./app/routes/main.js')(app, passport); 
-require('./app/routes/auth.js')(app, passport); 
+require("./app/routes/main.js")(app, passport);
+require("./app/routes/auth.js")(app, passport);
 
 let server;
-
 
 function runServer(DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
@@ -44,11 +42,12 @@ function runServer(DATABASE_URL, port = PORT) {
       if (err) {
         return reject(err);
       }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-        .on('error', err => {
+      server = app
+        .listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+        .on("error", err => {
           mongoose.disconnect();
           reject(err);
         });
@@ -59,7 +58,7 @@ function runServer(DATABASE_URL, port = PORT) {
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      console.log('Closing server');
+      console.log("Closing server");
       server.close(err => {
         if (err) {
           return reject(err);
@@ -76,9 +75,6 @@ if (require.main === module) {
 
 module.exports = { runServer, app, closeServer };
 
-
-
 // launch ======================================================================
 //app.listen(port);
-console.log('My favorite port is ' + port);
-
+console.log("My favorite port is " + port);
